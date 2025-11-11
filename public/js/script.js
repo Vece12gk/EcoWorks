d1c880813b7805d129ec0a8e5c15d45722a2e540
Vece12gk <natan18vece@gmail.com>
2025-11-10 22:48:28 -0300

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,8 @@
-
+function completarMissao(idMissao) {
+  fetch('php/addPoints.php?id=' + idMissao)
+    .then(res => res.text())
+    .then(msg => {
+      alert(msg);
+      location.reload();
+    });
+}
