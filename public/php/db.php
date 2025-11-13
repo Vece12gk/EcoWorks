544197c596403ef6d41f5c969e734a94c5293490
Vece12gk <natan18vece@gmail.com>
2025-11-12 22:11:50 -0300

    Change database connection settings
    Updated database connection parameters for production.

--- public/php/db.php
+++ public/php/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "ecotarefas";
+$host = "sql100.infinityfree.com";
+$user = "if0_40392783";
+$pass = "2DRgtPe92Q";
+$dbname = "if0_40392783_ecotarefas";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
